523c7fe4cc8bbbda81afa5aeeb737800c1e6ad00
Khushi-080 <khushi1354.becse24@chitkara.edu.in>
2025-02-11 20:51:27 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,17 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    int x;
+    scanf("%d",&x);
+    char y;
+    scanf("%c",&y);
+    if(y = a,e,i,o,u){
+        printf("%c",y);
+    }
+    else if{
+        printf("%c",y);
+    }
+    else{
+        printf("%d",x);
+    }
+}
