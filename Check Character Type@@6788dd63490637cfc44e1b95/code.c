ac3fdcb5fa88006118ab11fa4cec6121ead69a53
Khushi-080 <khushi1354.becse24@chitkara.edu.in>
2025-02-11 20:53:02 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -8,7 +8,7 @@ int main(){
     if(y = a,e,i,o,u){
         printf("%c",y);
     }
-    else if{
+    else if(y = b,c,d,f,g,h,j,k,l,m,n,p,q,r,t,v,w,x,y,z){
         printf("%c",y);
     }
     else{
